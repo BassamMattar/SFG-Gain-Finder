a24fc500a04ba8dfd0d628c167771285be0dc6df
ecc <ecc@DESKTOP-OB6T99G>
2018-04-22 22:22:56 +0200

    TIPS TO REMMEMBER

--- ControlGUI/src/sfg/CombinationsUtility.java
+++ ControlGUI/src/sfg/CombinationsUtility.java
@@ -6,7 +6,9 @@ public class CombinationsUtility {
 	//returns all combinations bigger than 1 and smaller than n
 	@SuppressWarnings("unchecked")
 	public ArrayList<ArrayList<Integer>>[] getAllCombinationsOfN(int n) {
-		ArrayList<ArrayList<Integer>>[] allCombinations = new ArrayList[n - 1];
+		ArrayList<ArrayList<Integer>>[] allCombinations = new ArrayList[n - 1];// remember negative index for simple case 2 nodes
+		//aha i forget to tell you that you calculate the parallel paths as the same !!!
+		//Sajed for hard cases only ^_^
     	for (int i = 0; i < allCombinations.length; i++) {
 			allCombinations[i] = new ArrayList<ArrayList<Integer>>();
 		}
